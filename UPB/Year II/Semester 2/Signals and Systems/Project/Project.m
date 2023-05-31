b9dbeace4e4267fa63ae3296d04da75c44beaa2f
Adrian Câmpean <37622282+bimj0rk@users.noreply.github.com>
2023-05-31 10:33:42 +0300

    S&S Project Done

--- UPB/Year II/Semester 2/Signals and Systems/Project/Project.m
+++ UPB/Year II/Semester 2/Signals and Systems/Project/Project.m
@@ -6,19 +6,17 @@
 x1 = 4*x11 + 2*x12 + 10*x13 + 11*x14 - 2*x15;
 plot(t1, x1), xlabel('Time'), ylabel('x1'), grid
 
-[x21, t2] = impuls1(-17, 15, 0.001, -10);
-[x22, t2] = impuls1(-17, 15, 0.001, -5);
-[x23, t2] = impuls1(-17, 15, 0.001, 4);
-[x24, t2] = impuls1(-17, 15, 0.001, 8);
+[x21, t2] = step1(-17, 15, 0.001, -10);
+[x22, t2] = step1(-17, 15, 0.001, -5);
+[x23, t2] = step1(-17, 15, 0.001, 4);
+[x24, t2] = step1(-17, 15, 0.001, 8);
 x2 = 2*x21 + 0*x22 - 3*x23 + 1*x24;
 plot(t2, x2), xlabel('Time'), ylabel('x2'), grid
 
 conv11 = conv(x1, x1);
-tconv11 = 2*t1(1):0.01:(2*t1(end));
-plot(tconv11, conv11), xlabel('Time'), ylabel('x1*x1');
-grid on;
+tconv11 = 2*(-30):0.001:2*34;
+plot(tconv11, conv11), xlabel('Time'), ylabel('x1*x1'); grid 
 
 conv12 = conv(x1, x2);
-tconv12 = -(30+17):0.01:(34+15);
-plot(tconv12, conv12), xlabel('Time'), ylabel('x1*x2');
-grid on;
+tconv12 = -(30+17):0.001:(34+15);
+plot(tconv12, conv12), xlabel('Time'), ylabel('x1*x2'); grid 
